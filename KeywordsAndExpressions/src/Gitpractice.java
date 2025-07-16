4c2f0887d6264ec424d94f8c17ffa8215b809d6b
keerthanaveganti <keerthanaveganti712@gmail.com>
2025-07-15 20:17:36 -0400

    seeing the final result after changes

--- KeywordsAndExpressions/src/Gitpractice.java
+++ KeywordsAndExpressions/src/Gitpractice.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
-public class gitpractice {
+public class Gitpractice {
+    public static void main(String[] args) {
+        System.out.println("Hello Keerthana from Git");
+    }
 }
